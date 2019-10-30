2860810394072b688efac1504d48462e9db08134
Rich Tabor <hi@richtabor.com>
2019-10-29 20:22:50 -0400

    Step 1A: Add author markup to edit function

--- src/edit.js
+++ src/edit.js
@@ -14,7 +14,20 @@ class AuthorEdit extends Component {
 		} = this.props;
 
 		return (
-			<div className={ className }>Hello World, (from the editor)</div>
+			<div className={ className }>
+				<figure className="wp-block-building-blocks-author__avatar">
+					<img className="wp-block-building-blocks-author__avatar-img" src="" alt="" />
+				</figure>
+
+				<div className="wp-block-building-blocks-author__content">
+					<span className="wp-block-building-blocks-author__name">
+						Rich Tabor
+					</span>
+					<span className="wp-block-building-blocks-author__biography">
+						Senior Product Manager, WordPress Experience at GoDaddy. Previously CoBlocks & ThemeBeans.
+					</span>
+				</div>
+			</div>
 		);
 	}
 }
